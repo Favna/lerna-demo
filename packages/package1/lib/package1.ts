5a8ce6e87a3fcbe58fd9f80c0b2c4aff28e1aa44
Jeroen Claassens <j.claassens@cgi.com>
2019-08-16 20:14:53 +0200

    feat(package1): create package1
    Package1 now has actual code that can be called
    
    BREAKING CHANGE: The dummy code is gone and any code based on it will no longer function

--- packages/package1/lib/package1.ts
+++ packages/package1/lib/package1.ts
@@ -1,5 +1,40 @@
-export const package1 = () => {
-  // TODO
+export type PackagerOptions = {
+  name: string;
+  details: string;
 };
 
-export default package1;
+export class Packager {
+  private static packageName: string;
+  private static packageDetails: string;
+
+  public constructor(options: PackagerOptions) {
+    this.name = options.name;
+    this.details = options.details;
+  }
+
+  public init() {
+    console.group();
+    console.log('the name that was set is: ', this.name);
+    console.log('this details that were set are: ', this.details);
+
+    return console.groupEnd();
+  }
+
+  public get name() {
+    return this.name;
+  }
+
+  public set name(name: string) {
+    this.name = name;
+  }
+
+  public get details(): string {
+    return this.details;
+  }
+
+  public set details(value: string) {
+    this.details = value;
+  }
+}
+
+export const packager = (options: PackagerOptions) => new Packager(options);
